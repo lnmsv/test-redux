d790c590c82274ada73f9ebf9209e8c0a25d171f
piopump <>
2020-06-29 01:44:06 +0300

    :fire:

---
+++ index.js
@@ -0,0 +1,25 @@
+const redux = require('redux');
+
+const initialState = {
+    views: 0,
+    likes: 0,
+};
+const reducer = (state, action) => {
+    switch (action.type) {
+        case 'ADD_VIEW':
+            return Object.assign({}, state, { views: state.views + 1});
+        case 'ADD_LIKE':
+            return Object.assign({}, state, { likes: state.likes + 1});
+        default:
+            return state;
+    }
+};
+const store = redux.createStore(reducer, initialState);
+
+store.subscribe(() => console.log(store.getState()));
+
+store.dispatch({type: 'ADD_VIEW'});
+store.dispatch({type: 'ADD_VIEW'});
+store.dispatch({type: 'ADD_LIKE'});
+store.dispatch({type: 'ADD_VIEW'});
+store.dispatch({type: 'ADD_LIKE'});
